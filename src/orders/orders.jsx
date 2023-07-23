11f5c792461f31b505fa6fd65363a77c8801b1e5
BavleyAdel <119805780+BavleyAdel@users.noreply.github.com>
2023-07-23 15:15:55 +0300

    Update orders.jsx

--- src/orders/orders.jsx
+++ src/orders/orders.jsx
@@ -1,30 +1,28 @@
 import React from "react";
 import './orders.css'
-import cart from '../images/cart.png'
 
-const Orders=({orders , deleteOrder})=>{
-    return(
+const Orders = ({ orders, deleteOrder }) => {
+    return (
         <>
-                        <span className="cart">
-                            <img src={cart} alt="" className="cart"/>
-                        </span>
-                        <div className='selected-products'>
-                            {
-                                orders?.map((sp, idx) =>
-                                    <div key={idx}>
-                                        <img src={sp.img} alt='' />
-                                        <div className="details">
-                                            <h3>{sp.title}</h3>
-                                            <p><sub>EGP </sub>{sp.price}</p>
-                                            <p><del>{sp.old}</del>{sp.off}% off</p>
-                                        </div>
-                                        <button className="btn" onClick={(e) => deleteOrder(sp, e, idx)}>Delete</button>
-                                        <hr />
-                                    </div>
-                                )
-                            }
+            <span className="cart material-symbols-rounded">
+                shopping_cart</span>
+            <div className='selected-products'>
+                {
+                    orders?.map((sp, idx) =>
+                        <div key={idx}>
+                            <img src={sp.img} alt='' />
+                            <div className="details">
+                                <h3>{sp.title}</h3>
+                                <p><sub>EGP </sub>{sp.price}</p>
+                                <p><del>{sp.old}</del>{sp.off}% off</p>
+                            </div>
+                            <button className="btn" onClick={(e) => deleteOrder(sp, e, idx)}>Delete</button>
+                            <hr />
                         </div>
-                        
+                    )
+                }
+            </div>
+
         </>
     )
 }
